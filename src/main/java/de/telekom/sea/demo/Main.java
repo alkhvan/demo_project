38f3009199a98f6ca591c468b81b07d8d6f0b236
Aleksandra Khvan <aleksandra.klochko@t-systems.com>
2021-06-28 11:46:19 +0300

    slider in progress html

--- src/main/java/de/telekom/sea/demo/Main.java
+++ src/main/java/de/telekom/sea/demo/Main.java
@@ -1,7 +1,12 @@
 package de.telekom.sea.demo;
 
-public class Main {
-    public static void main(String[] args) {
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+public static void main(String[] args) {
+System.out.println("hi");
     }
 }
